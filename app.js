d63476ee87d9ca4de3c7de9752d92e310ce5e20a
XZohaib01 <162486992+XZohaib01@users.noreply.github.com>
2024-03-06 10:11:36 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-var guest_list = ["Aleem Chohan", "Muneeb Chohan", "Zohaib Chohan"];
+var guest_list = ['Aleem Chohan', 'Muneeb Chohan', 'Zohaib Chohan'];
 for (var i = 0; i < guest_list.length; i++) {
-    console.log("Respected brother" + guest_list[i] + ",/n/nWe invited you on dinner Tomorrow./n/nThank You");
+    console.log('Respected brother' + guest_list[i] + ',/n/nWe invited you on dinner Tomorrow./n/nThank You');
 }
+var not_present = 'Aleem Chohan';
+var new_guest = 'Elon Musk';
+guest_list[0] = new_guest;
+for (var i = 0; i < guest_list.length; i++) {
+    console.log('Respected brother' + guest_list[i] + ',/n/nWe invited you on dinner Tomorrow./n/nThank You');
+}
+console.log("Mr. ".concat(not_present, " Will Not Comming Tomorrow Dinner. "));
--- app.ts
+++ app.ts
@@ -1,4 +1,11 @@
-let guest_list : string []=[`Aleem Chohan`,`Muneeb Chohan`,`Zohaib Chohan`];
+let guest_list : string []=['Aleem Chohan','Muneeb Chohan','Zohaib Chohan'];
 for(let i=0; i<guest_list.length; i++){
-    console.log(`Respected brother` + guest_list[i] +`,/n/nWe invited you on dinner Tomorrow./n/nThank You`)
+    console.log('Respected brother' + guest_list[i] +',/n/nWe invited you on dinner Tomorrow./n/nThank You')
 }    
+let not_present : string = 'Aleem Chohan';
+let new_guest : string = 'Elon Musk';
+guest_list[0] = new_guest;
+for(let i=0; i<guest_list.length; i++){
+    console.log('Respected brother' + guest_list[i] +',/n/nWe invited you on dinner Tomorrow./n/nThank You')
+}    
+console.log(`Mr. ${not_present} Will Not Comming Tomorrow Dinner. `)
