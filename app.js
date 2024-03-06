23f1658aa7f39e702052ed2ed924d04195571c1d
XZohaib01 <162486992+XZohaib01@users.noreply.github.com>
2024-03-06 10:12:16 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var guest_list = ['Aleem Chohan', 'Muneeb Chohan', 'Zohaib Chohan'];
-for (var i = 0; i < guest_list.length; i++) {
-    console.log('Respected brother' + guest_list[i] + ',/n/nWe invited you on dinner Tomorrow./n/nThank You');
-}
+// for(let i=0; i<guest_list.length; i++){
+//     console.log('Respected brother' + guest_list[i] +',/n/nWe invited you on dinner Tomorrow./n/nThank You')
+// }    
 var not_present = 'Aleem Chohan';
 var new_guest = 'Elon Musk';
 guest_list[0] = new_guest;
@@ -9,3 +9,7 @@ for (var i = 0; i < guest_list.length; i++) {
     console.log('Respected brother' + guest_list[i] + ',/n/nWe invited you on dinner Tomorrow./n/nThank You');
 }
 console.log("Mr. ".concat(not_present, " Will Not Comming Tomorrow Dinner. "));
+guest_list.unshift('Yaseen', 'Asim', 'Uzair');
+for (var i = 0; i < guest_list.length; i++) {
+    console.log('Respected brother' + guest_list[i] + ',/n/nWe invited you on dinner Tomorrow. We Found Big Tbale So We Decided  To Invite Three More Guest/n/nThank You');
+}
--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
 let guest_list : string []=['Aleem Chohan','Muneeb Chohan','Zohaib Chohan'];
-for(let i=0; i<guest_list.length; i++){
-    console.log('Respected brother' + guest_list[i] +',/n/nWe invited you on dinner Tomorrow./n/nThank You')
-}    
+// for(let i=0; i<guest_list.length; i++){
+//     console.log('Respected brother' + guest_list[i] +',/n/nWe invited you on dinner Tomorrow./n/nThank You')
+// }    
 let not_present : string = 'Aleem Chohan';
 let new_guest : string = 'Elon Musk';
 guest_list[0] = new_guest;
@@ -9,3 +9,7 @@ for(let i=0; i<guest_list.length; i++){
     console.log('Respected brother' + guest_list[i] +',/n/nWe invited you on dinner Tomorrow./n/nThank You')
 }    
 console.log(`Mr. ${not_present} Will Not Comming Tomorrow Dinner. `)
+guest_list.unshift('Yaseen','Asim','Uzair');
+for(let i=0; i<guest_list.length; i++){
+    console.log('Respected brother' + guest_list[i] +',/n/nWe invited you on dinner Tomorrow. We Found Big Tbale So We Decided  To Invite Three More Guest/n/nThank You')
+}    
