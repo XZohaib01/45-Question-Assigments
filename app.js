31cee2841a738430207cacfd7bb723e229776e45
XZohaib01 <162486992+XZohaib01@users.noreply.github.com>
2024-03-06 09:46:59 -0800

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-// lowe"r case
-var personName = "Zohaib";
-console.log("lowercase:", personName.toLocaleLowerCase());
-// upper case
-console.log("uppercase:", personName.toLocaleUpperCase());
-// Tittle case
-console.log("tittlecase:", personName.replace(/\bw/g, function (c) { return c.toLocaleUpperCase(); }));
