04ed0aed8f8f3400ab0a92a09007324bfafcf0dd
XZohaib01 <162486992+XZohaib01@users.noreply.github.com>
2024-03-06 09:53:24 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
-var personName = "Zohaib Chohan";
-console.log("hellow ".concat(personName, " would you like to learn some Python today?"));
+// lowe"r case
+var personName = "Zohaib";
+console.log("lowercase:", personName.toLocaleLowerCase());
+// upper case
+console.log("uppercase:", personName.toLocaleUpperCase());
+// Tittle case
+console.log("tittlecase:", personName.replace(/\bw/g, function (c) { return c.toLocaleUpperCase(); }));
--- app.ts
+++ app.ts
@@ -1,2 +1,10 @@
-let personName: string= "Zohaib Chohan";
-console.log(`hellow ${personName} would you like to learn some Python today?`);
+// lowe"r case
+
+let personName: string="Zohaib" 
+console.log("lowercase:", personName.toLocaleLowerCase());
+
+// upper case
+console.log("uppercase:", personName.toLocaleUpperCase());
+
+// Tittle case
+console.log("tittlecase:", personName.replace(/\bw/g,c => c.toLocaleUpperCase()));
